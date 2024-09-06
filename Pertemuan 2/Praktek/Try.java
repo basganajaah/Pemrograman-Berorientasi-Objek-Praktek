c13b139b460e8a067e1d01c1fad5ea5610fe69da
basganajaah <bandyagaadiansyah@gmail.com>
2024-09-07 00:24:00 +0700

    push pertemuan 3

--- Pertemuan 2/Praktek/Try.java
+++
@@ -1,9 +0,0 @@
-import java.awt.desktop.SystemEventListener;
-
-public class Try {
-    public static void main (String[] args) {
-        System.out.println("Aku kamu");
-        String string1 = "Aku juga";
-        System.out.println(string1);
-    }
-}
