e23e766210756d15b871eb0147487ab82a04fbbc
basganajaah <bandyagaadiansyah@gmail.com>
2024-12-04 22:18:39 +0700

    edit tugas java design pattern

--- Pertemuan 14/StrategyPattern/src/StrategyPatternMain.java
+++ Pertemuan 14/StrategyPattern/src/StrategyPatternMain.java
@@ -2,15 +2,12 @@ public class StrategyPatternMain {
     public static void main(String[] args) {
         PaymentContext context = new PaymentContext();
 
-        // Menggunakan Credit Card
         context.setPaymentStrategy(new GopayPayment());
         context.pay(150000);
 
-        // Beralih ke PayPal
         context.setPaymentStrategy(new ShopeepayPayment());
         context.pay(225000);
 
-        // Beralih ke Bank Transfer
         context.setPaymentStrategy(new DanaPayment());
         context.pay(138500);
     }
